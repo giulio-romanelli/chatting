9357390247da93c4aecfeb34ebbf41b5060def31
giulio-romanelli <153216233+giulio-romanelli@users.noreply.github.com>
2023-12-07 21:04:40 +0000

    packages

--- Hello.py
+++ Hello.py
@@ -12,8 +12,53 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Imports
+from pygame import mixer
+from openai import OpenAI
 import streamlit as st
 from streamlit.logger import get_logger
+import time
+import os
+
+##-------------------------------------------------------------------------------------##
+## playAudio
+##-------------------------------------------------------------------------------------##   
+def playAudio(filename, background=False):
+
+    mixer.init()
+    mixer.music.load(filename)
+    mixer.music.play()
+    while not background:
+        if mixer.music.get_busy() == False:
+            #mixer.quit()
+            break
+
+##-------------------------------------------------------------------------------------##
+## stopAudio
+##-------------------------------------------------------------------------------------##   
+def stopAudio( ):
+    if mixer.get_init():
+        mixer.music.stop()
+        mixer.quit()
+
+##-------------------------------------------------------------------------------------##
+## textToSpeechGoogle
+##-------------------------------------------------------------------------------------##
+def textToSpeechGoogle(text, filename, language="en"):
+
+    # Check audio file is not open
+    while True:
+        try:
+            myfile = open(filename, "wb")
+            break                             
+        except IOError:
+            stopAudio( )
+
+    speech = gTTS(text, lang=language)
+    speech.save(filename)
+
+
+
 
 LOGGER = get_logger(__name__)
 
